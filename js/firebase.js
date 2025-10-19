7fa6d6d9d056b31f13cdfe89af2dbb7affc895e9
AlxT259 <alxt259@gmail.com>
2025-10-19 22:46:36 +0300

    Minor changes to firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-// ⚙️ Конфіг твого проєкту:
+
 const firebaseConfig = {
   apiKey: "AIzaSyC7KoFyjclpBmd9HxkaAwmd9H0uqPfKfxQ",
   authDomain: "test-repo-admin.firebaseapp.com",
@@ -11,8 +11,7 @@ const firebaseConfig = {
   appId: "1:1040790855579:web:8958fe9573634861a0cb3e"
 };
 
-// 🚀 Ініціалізація Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// 💾 Підключення Firestore
 export const db = getFirestore(app);
